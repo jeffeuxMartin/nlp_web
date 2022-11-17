fa9e6a6fc83afb11265145344c581db208f2d074
shenzizi <esperanzayaya22@gmail.com>
2022-11-17 08:22:28 +0800

    docs: modify comments

--- lab/pokemons/src/App.js
+++ lab/pokemons/src/App.js
@@ -203,12 +203,16 @@ function App() {
 =======
   
   /**
-   *  較好的方式是只存 {id: id, count: count} ，不存多餘的資訊（e.g., price）
+   *  較好的方式是 cart 內只存 {id: id, count: count} ，不存多餘的資訊（e.g., price）
    *  因為 id 應該要是 unique，count 是使用者操作過後的值
    *  然後再回查資料來源： pokemons 列表 or pokemon data ，找到要呈現在 cart 裡對應的資訊
    *  但因為 pokemons 列表裡目前只有 name & url，
+<<<<<<< HEAD
    *  所以這裡就先取巧，直接在 handleAddToCart 理傳入 cart 呈現時需要的所有資料
 >>>>>>> 3dcca5a7 (feat: add cart info)
+=======
+   *  所以這裡就先取巧，直接在 handleAddToCart 裡傳入 cart 呈現時需要的所有資料
+>>>>>>> 1eb4fe17 (docs: modify comments)
    */
    const [cart, setCart] = useState([]);
 
@@ -328,6 +332,7 @@ function App() {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
       console.log('updatedCart');
 >>>>>>> 3dcca5a7 (feat: add cart info)
@@ -345,6 +350,8 @@ function App() {
 =======
 >>>>>>> ae4cf03e (feat: add cart info)
 =======
+>>>>>>> 09bba9ba (docs: modify comments)
+=======
       console.log('updatedCart');
 >>>>>>> 3dcca5a7 (feat: add cart info)
 =======
@@ -352,6 +359,8 @@ function App() {
 =======
       console.log('updatedCart');
 >>>>>>> 3dcca5a7 (feat: add cart info)
+=======
+>>>>>>> 1eb4fe17 (docs: modify comments)
       setCart(updatedCart)
 >>>>>>> 0684ec83 (feat: add cart info)
 >>>>>>> 6c99dfdf (feat: add cart info)
